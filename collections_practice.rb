9ed8f665c02183d9da0160020b78440053174bcf
Wasif Khan <waasif.khaan@gmail.com>
2018-05-08 21:28:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,5 @@ def swap_elements(array)
   array[0], array[1], array[2] = array[0], array[2], array[1]
   array
 end
+def swap_elements_from_to(array,index,destination_index)
+  array.collect.with_index do | |
