208b31450b8c15490fed83a9f2c9aa0a14ee6e1a
Wasif Khan <waasif.khaan@gmail.com>
2018-05-08 22:21:07 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,7 @@ end
 def add_s(array)
   array.each_with_index.collect do |element, index|
     element << "s"
-  element = element if index == 1
+  element = element if index == "1"
   end
 end
     
